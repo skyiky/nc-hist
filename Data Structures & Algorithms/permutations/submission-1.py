5847edffdcb94c2bb8d2891748e2fa8c7e8592da
David Zhang <davidzhang@microsoft.com>
2026-09-08 19:14:08 -0700

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Save? 
+    # Choose?  
+    # Stop?
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
+                return
+
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+
+
+        backtrack(0)
+        return list(result)
