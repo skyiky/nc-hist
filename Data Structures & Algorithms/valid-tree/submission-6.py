186cdc0856335581de47933c095cc883e8209897
David Zhang <davidzhang@microsoft.com>
2026-09-22 19:40:26 -0700

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    # Pattern 2: 
+    # An undirected graph is a tree if it is connected and has exactly n - 1 edges.
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+
+        graph = [[] for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        visited = set()
+        def dfs(node):
+            visited.add(node)
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        dfs(0)
+        return len(visited) == n
